6cc3940a9d4f02798c6be354e46fd080c3cffeca
Eduardo Faustino <83729062+Faustinooo@users.noreply.github.com>
2023-06-01 15:30:55 -0300

    DELETE FUNÇÃO GERADOR

--- MOD/__init__.py
+++ MOD/__init__.py
@@ -20,34 +20,6 @@ def contacpf2(a = ""):
         contador -= 1
     conta = (saida * 10) % 11
     return conta
-def gerador(a = 1):
-    lista = []
-    for i in range(1):
-        c = ""
-        for i in range(9):
-            a = r.randint(0, 9)
-            b = str(a)
-            c += b
-        e = contacpf(c)
-        if int(e) > 9:
-            e = 0
-        d = c + str(e)
-        g = contacpf2(d)
-        if int(g) > 9:
-            g = 0
-        f = d + str(g)
-        contador = 1
-        for i in f:
-            lista.append(i)
-            if contador == 3 or contador == 6:
-                lista.append(".")
-            if contador == 9:
-                lista.append("-")
-            contador += 1
-    fim = ""
-    for i in lista:
-        fim += i
-    return fim
 
 def linha(a = 0):
     print(f"\033[1;{a}m-=\033[m" * 20)
